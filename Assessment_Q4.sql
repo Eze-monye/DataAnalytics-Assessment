b121b8075122ca47ca369885374122f357d84a0b
Eze-monye <120463705+Eze-monye@users.noreply.github.com>
2025-05-21 10:19:33 +0100

    Update Assessment_Q4.sql

--- Assessment_Q4.sql
+++ Assessment_Q4.sql
@@ -1,20 +1,30 @@
 SELECT
     u.id AS customer_id,
-    u.name,
+    concat (u.first_name, ' ', u.last_name) as name ,
+    
+    -- Calculates how many full months a customer has been active
     TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()) AS tenure_months,
-	--calculates customers total transactions and substitues zero for customers without transactions
-    COALESCE(COUNT(sa.id), 0) AS total_transactions,
-	--calculates the clv and converts the 0.1 percent to 0.001
+
+    -- Total number of transactions (0 for users with no transactions)
+    COUNT(sa.id) AS total_transactions,
+
+    -- Calculates estimated CLV: average monthly transactions * 12 * 0.1% of avg confirmed amount
     CASE
         WHEN TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()) = 0 THEN NULL
-        ELSE ((COUNT(sa.id) / TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE())) * 12) * AVG(sa.confirmed_amount * 0.001)
+        ELSE (
+            (COUNT(sa.id) / TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE())) * 12
+        ) * AVG(sa.confirmed_amount * 0.001)
     END AS estimated_clv
+
 FROM
     users_customuser u
+
+-- Left join to include users with zero transactions
 LEFT JOIN
-    savings_savingsaccount sa ON sa.owner_id = u.id
+    savings_savingsaccount sa ON sa.owner_id = u.id AND sa.confirmed_amount > 0
+
 GROUP BY
-    u.id, u.name, u.date_joined
+    u.id, concat (u.first_name, ' ', u.last_name), u.date_joined
+
 ORDER BY
-    estimated_clv DESC
-	
+    estimated_clv DESC;
